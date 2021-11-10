225747c0d0a471221d20bfa985222b7b6ddff513
Andrey Testov <avtestov@ya.ru>
2021-11-10 19:52:42 +0300

    Инициализация бота

---
+++ bot.py
@@ -0,0 +1,10 @@
+from aiogram import Bot, Dispatcher, executor, types
+
+# Объект бота
+bot = Bot(token=open("bot-token.txt", "r").read())
+# Диспетчер для бота
+dp = Dispatcher(bot)
+
+if __name__ == "__main__":
+    # Запуск бота
+    executor.start_polling(dp, skip_updates=True)
