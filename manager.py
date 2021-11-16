2dec605d4ca44b7b72eca9e2dd2348d43f503010
Andrey Testov <avtestov@ya.ru>
2021-11-16 16:52:36 +0300

    Добавил метод search в класс Manager

--- manager.py
+++ manager.py
@@ -6,6 +6,10 @@ class Manager():
     '''
     Класс Manager реализует взаимодействие пользователей с базой.
     Предостовляет методы для бота, но не логику бота.
+    Методы возвращают: 
+        -1: доступ запрещен
+        True: операция выполнена успешно
+        Данные: если запрашивались данные
     '''
     product = Product()
     users = Users()
@@ -15,17 +19,32 @@ class Manager():
 
     def isBan(self, user):
         return not self.checkRight(user, 'access')
+
+    def getDate(self, user):
+        return users[user].getDate()
     
-    def addProducts(self, user, date, file):
+    def addProducts(self, user, file, date=False):
         if not self.checkRight(user, 'change'):
-            return False
-        
+            return -1
+
+        date = self.getDate(user) if not date else date
+
         self.product.Add(date, file)
         return True
 
-    def updateProducts(self, user, date, file, rewrite = True):
+    def updateProducts(self, user, file, date = False, rewrite = True):
         if not self.checkRight(user, 'change'):
-            return False
-        
+            return -1
+
+        date = self.getDate(user) if not date else date
+
         self.product.Update(date, file, rewrite)
         return True
+
+    def search(self, user, request, data=False]):
+        if not self.checkRight(user, 'reading'):
+            return -1
+
+        date = self.getDate(user) if not date else date
+
+        return self.product.search(date, request)
