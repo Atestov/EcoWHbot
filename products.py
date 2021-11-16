f34d478437b545e13c987ca2349cb2ee56b03966
Andrey Testov <avtestov@ya.ru>
2021-11-16 13:34:04 +0300

    Добавил функцию поиска
    Привел названия функций к одному типу написания

--- products.py
+++ products.py
@@ -21,7 +21,7 @@ class Products():
             pd.DataFrame([],columns=self.columns).to_csv(dataFile, index=False)
         return dataFile
 
-    def _ExtractData_(self, date, file):
+    def __extractData__(self, date, file):
         file = pd.read_excel(file)
         # У файлов с отстаками плавающее начало таблицы.
         #Иногда есть заголовок, иногда нет. Ищем начало по слову Цена
@@ -36,19 +36,19 @@ class Products():
         data = data.fillna(0)#Nan -> 0
         data.insert(0, "date", date) #Вставка даты
         data.columns = self.columns
-        data['name'] = self._ReplaceSimilarCharacters_(data['name'])
+        data['name'] = self.__replaceSimilarCharacters__(data['name'])
         return data
         
-    def Add(self, date, file):
+    def add(self, date, file):
         '''
         Добавляет записи за указанную дату.
         В отличие от Update не меняет существующие записи
         '''
-        data = self._ExtractData_(date, file)
+        data = self.__extractData__(date, file)
         self.data = pd.concat([self.data,data])
         self.Save()
         
-    def Update(self, date, file, rewriteAll = True):
+    def update(self, date, file, rewriteAll = True):
         '''
         Обновляет записи за определенную дату.
         Если параметр rewriteAll = True то полностью заменяет записи за указанную дату
@@ -56,21 +56,21 @@ class Products():
         '''
         if rewriteAll:
             self.data = self.data.loc[self.data['date'] != date]
-        data = self._ExtractData_(date, file)
+        data = self.__extractData__(date, file)
         #Добавляем все записи и удаляем дубликаты по дате и названию
         self.data = pd.concat([self.data, data]).drop_duplicates(['date','name'], keep='last')
         self.Save()
         
-    def Save(self):
+    def save(self):
         self.data.to_csv(self._createDataFile_(), index=False)
 
-    def Load(self):
+    def load(self):
         self.data = pd.read_csv(self._createDataFile_())
 
-    def _ReplaceSimilarCharacters_(self, df):
+    def __replaceSimilarCharacters__(self, df):
         '''
         Замена похожих символов и исправление опечаток.
-        На вход принимает DataFrame, возвращает обработанный DataFrame
+        На вход принимает столбец DataFrame, возвращает обработанный столбец DataFrame
         '''
         #В исходной базе 1с и соответственно выгрузке из неё много опечаток и разных написаний
         #Например размер горшка С25 может быть записан как С25, С 25, С25 см, С 25 см см
@@ -106,3 +106,19 @@ class Products():
         df = df.str.replace(r'\s{2,}', " ", regex = True) #Убираем множественные пробелы
         
         return df
+    
+    def search(self, date, request):
+        '''
+        Функция принимает дату и строку поиска и возвращает DataFrame содержащий подходящие данные.
+        Строка разбивается на слова (словом считается текст между пробелом/пробелами)
+        Возвращаются только те значения где есть все слова.
+        '''
+        # Насколько я понял в pandas нет встроенного средства искать все подстроки одновременно
+        # Но есть регулярные выражения и Positive Lookahead
+
+        request = self.__replaceSimilarCharacters__(pd.DataFrame([request], columns=['r'])['r'])[0].split() #Убираем опечатки
+        #Какая то громоздкая конструкция получается. Преобразование в DataFrame, обработка, преобразование в строку, разделение на слова.
+        
+        res = self.data.loc[self.data['date']==date]
+        #''.join('(?=.*{0})'.format(t) for t in request) -> '(?=.*запрос)(?=.*информации)(?=.*к)(?=.*базе)'
+        return res.loc[res['name'].str.contains(''.join('(?=.*{0})'.format(t) for t in request), regex=True)]
